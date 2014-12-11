7be23ca491e3a3c24841041fa269741b76e7e2a9
marekczuma <marek.czuma@gmail.com>
2014-12-11 23:30:40 +0100

    działająca wersja - dodawane są: tekst, notatki, karteczki dodają siępod spodem

---
+++ Unity-test/Assets/Scripts/ButtonAddNoteScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class ButtonAddNoteScript : MonoBehaviour {
+	//public GameObject stickyNote;
+	public GameObject kanwas;
+	public GameObject note;
+	public InputField fieldOfText;
+
+	private Vector3 nextNotePosition;
+	// Use this for initialization
+	void Start () {
+		nextNotePosition = this.transform.position;
+		nextNotePosition.y -= 110;
+	}
+	public void AddNote()
+	{
+		//Instantiate (stickyNote);
+
+		GameObject tmpNote = Instantiate (note, nextNotePosition, Quaternion.identity) as GameObject;
+		tmpNote.transform.SetParent (kanwas.transform);
+		tmpNote.transform.GetChild (0).GetComponent<Text> ().text = fieldOfText.text ;
+		CalculateNextPosition ();
+	}
+	void CalculateNextPosition()
+	{
+		nextNotePosition.y -= 160;
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
